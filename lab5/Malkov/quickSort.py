d98a4d6dcd86d48f24daec221da9eac939643130
paulmalk <paulmalk.v@gmail.com>
2015-09-26 19:51:15 +0400

    Update quickSort.py

--- lab5/Malkov/quickSort.py
+++ lab5/Malkov/quickSort.py
@@ -1,22 +1,23 @@
-from sys import stdin
+from sys import stdin, stdout
 import random
 
-def quickSort(arr):
-    less = []
-    equal = []
-    more = []
-    if len(arr) < 2:
-        return arr
+def quick_sort(start, end):
+    if start < end:
+        middle = partition(start, end)
+        quick_sort(start, middle - 1)
+        quick_sort(middle + 1, end)
+
+def partition(start, end):
     pivot = arr[random.randint(0, len(arr) - 1)]
-    for i in range(len(arr)):
-        if arr[i] < pivot:
-            less.append(arr[i])
-        elif arr[i] == pivot:
-            equal.append(arr[i])
-        else:
-            more.append(arr[i])
-    return quickSort(less) + equal + quickSort(more)
+    pivot, arr[end] = arr[end], pivot
+    i = start - 1
+    for j in range(start, end):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[end] = arr[end], arr[i + 1]
+    return i + 1
 
 arr = [int(i) for i in stdin.readline().split()]
-arr = quickSort(arr)
-print(arr)
+quick_sort(0, len(arr) - 1)
+stdout.write(" ".join(map(str, arr)))
