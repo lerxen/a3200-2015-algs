b4977b07025dc1e09de9dfa00e52758b4a9a9524
Valeriya <lerxen13@yandex.ru>
2015-10-17 12:49:09 +0300

    Update radix.py

--- lab6/Grushitcyna/radix.py
+++ lab6/Grushitcyna/radix.py
@@ -3,15 +3,13 @@ import math
 import sys
 
 
-
-
 def i_digit(n, i):
-    d = n % (10 ** (i + 1)) / (10 ** i)
+    d = (abs(n)) % (10 ** (i + 1)) / (10 ** i)
     return d
 
 
 def h_m_digits(n):
-    t = int(math.log10(n) + 1)
+    t = int(math.log10(abs(n)) + 1)
     return t
 
 
@@ -40,11 +38,25 @@ def counting_sort(array, f):
 
 def radix_sort(array):
     if not array:
-        return [];
-    n = h_m_digits(maximum(array))
+        return []
+    negatives = []
+    positives = []
+    for i in range(len(array)):
+        if array[i] < 0:
+            negatives.append(abs(array[i]))
+        else:
+            positives.append(array[i])
+    n = h_m_digits(maximum(positives))
+    m = h_m_digits(maximum(negatives))
     for i in range(0, n):
-        array = counting_sort(array, i)
-    return array
+        positives = counting_sort(positives, i)
+    for i in range(0, m):
+        negatives = counting_sort(negatives, i)
+    negatives.reverse()
+    for i in range(len(negatives)):
+        negatives[i] *= -1
+    return negatives + positives
+
 
 if __name__ == '__main__':
     elements = [int(s) for s in stdin.readline().split()]
