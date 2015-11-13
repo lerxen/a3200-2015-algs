b3b5fe8d7ee61b665466c3fd462baab46562e379
paulmalk <paulmalk.v@gmail.com>
2015-11-13 15:24:23 +0400

    Update quickSort.py

--- lab5/Malkov/quickSort.py
+++ lab5/Malkov/quickSort.py
@@ -2,22 +2,27 @@ from sys import stdin, stdout
 import random
 
 def quick_sort(start, end):
-    if start < end:
-        middle = partition(start, end)
-        quick_sort(start, middle - 1)
-        quick_sort(middle + 1, end)
-
-def partition(start, end):
-    pivot = arr[random.randint(0, len(arr) - 1)]
-    pivot, arr[end] = arr[end], pivot
-    i = start - 1
-    for j in range(start, end):
-        if arr[j] < pivot:
+    if end > start + 1:
+        left, right = partition(start, end, arr[random.randint(start, end)])
+        quick_sort(start, left)
+        quick_sort(right + 1, end)
+    return arr
+    
+def partition(start, end, pivot):
+    left = i = start
+    right = end - 1
+    while i <= right:
+        if arr[i] < pivot:
+            arr[i], arr[left] = arr[left], arr[i]
+            left += 1
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[end] = arr[end], arr[i + 1]
-    return i + 1
-
-arr = [int(i) for i in stdin.readline().split()]
-quick_sort(0, len(arr) - 1)
+        elif arr[i] > pivot:
+            arr[i], arr[right] = arr[right], arr[i]
+            right -= 1
+        else:
+            i += 1
+    return left, right
+    
+arr = [int(s) for s in stdin.readline().split()]
+quick_sort(0, len(arr))
 stdout.write(" ".join(map(str, arr)))
