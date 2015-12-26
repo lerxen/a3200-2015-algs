e07c2c0ee9ce389760a081d427504e442fe31bdd
Valeriya <lerxen13@yandex.ru>
2015-12-26 13:36:30 +0300

    Update palindrom.py
    corrected some mistakes

--- lab19/Grushitcyna/palindrom.py
+++ lab19/Grushitcyna/palindrom.py
@@ -1,7 +1,7 @@
 def find_palindrom(s):
     j = len(s)
     matrix = [[0 for k in xrange(j)] for i in xrange(j)]
-    matrix2 = [[0 for k in xrange(j)] for i in xrange(j)]
+    matrix2 = [[(0, 0) for k in xrange(j)] for i in xrange(j)]
     for m in xrange(j):
         matrix[m][m] = 1
     for l in xrange(1, j + 1):
@@ -16,22 +16,20 @@ def find_palindrom(s):
                 else:
                     matrix[m + l][m] = matrix[m + l][m + 1]
                     matrix2[m + l][m] = (m + l, m + 1)
-
-    return matrix[j - 1][0], matrix2
+    return matrix[j - 1][0], max_polindrom(matrix, matrix2, s)
 
 
 def max_polindrom(matrix, matrix2, s):
     result = ''
-    j = len(s)
+    j = len(s) - 1
     i = 0
-    while():
-        a, b = matrix2[j - 1][0]
-        if matrix[j - 1][0] != matrix[a][b]:
+    while j > len(s)/2:
+        a, b = matrix2[j][i]
+        if matrix[j][i] != matrix[a][b]:
             result += s[i]
-        i = a
-        j = b
+        i = b
+        j = a
+    return result
 
 
-a, b = find_palindrom('abaccba')
-print(a)
-print(b)
+print(find_palindrom('arosaupalanalapuasora'))
